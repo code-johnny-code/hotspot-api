dd0a1eee8e770b8fa5c26f8deac419067c203887
John Ross <john.ross86@live.com>
2020-03-27 07:16:02 -0500

    added endpoints

--- app.js
+++ app.js
@@ -9,8 +9,24 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
 
+// {"userId": "", "key": ""}
+app.post('/userLocations', (req, res) => {
+  Mongo.getUserLocations(req.body, (results) => res.send(results))
+});
+
+// {"accuracy": 1, "altitude": 1, "heading": 0, "key": "", "latitude": 12.34, "longitude": -12.347, "speed": 0, "userId": ""}
 app.post('/logPosition', (req, res) => {
   Mongo.logPosition(req.body, (results) => res.send(results));
+});
+
+// {"userId": "", "key": "", "timestamp": 1234567890}
+app.post('/reportPositive', (req, res) => {
+  Mongo.reportPositiveTest(req.body, (results) => res.send(results));
+});
+
+// {"userId": "", "key": ""}
+app.post('/reportNegative', (req, res) => {
+  Mongo.reportNegativeTest(req.body, (results) => res.send(results));
 });
 
 app.listen(process.env.PORT || 5000, function () {
--- mongoAPI.js
+++ mongoAPI.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const {geoToH3, kRing} = require('h3-js');
+const {geoToH3, kRing, h3SetToMultiPolygon} = require('h3-js');
 
 const MongoClient = require('mongodb').MongoClient;
 const client = new MongoClient(process.env.DB_URL, { useNewUrlParser: true, auth: {
@@ -25,4 +25,46 @@ module.exports = {
       response({'error': 'Unauthorized'})
     }
   },
+  getUserLocations(data, response) {
+    if (data.key === process.env.API_KEY) {
+      delete data.key;
+      return client.connect(function() {
+        const db = client.db(dbName);
+        const collection = db.collection('position_logs');
+        const userLocationCursor = collection.find({userId: data.userId});
+        const locations = [];
+        userLocationCursor.forEach(loc => locations.push(loc), () => response(locations));
+      });
+    } else {
+      response({'error': 'Unauthorized'})
+    }
+  },
+  reportPositiveTest(data, response) {
+    if (data.key === process.env.API_KEY) {
+      delete data.key;
+      return client.connect(function() {
+        const db = client.db(dbName);
+        const collection = db.collection('users');
+        collection.findOneAndUpdate({userId: data.userId}, {$set: {positive: true, positiveDTG: data.timestamp}}, (err, result) => {
+          response(result);
+        }, () => client.close());
+      });
+    } else {
+      response({'error': 'Unauthorized'})
+    }
+  },
+  reportNegativeTest(data, response) {
+    if (data.key === process.env.API_KEY) {
+      delete data.key;
+      return client.connect(function() {
+        const db = client.db(dbName);
+        const collection = db.collection('users');
+        collection.findOneAndUpdate({userId: data.userId}, {$set: {positive: false, positiveDTG: null}}, (err, result) => {
+          response(result);
+        }, () => client.close());
+      });
+    } else {
+      response({'error': 'Unauthorized'})
+    }
+  }
 };
